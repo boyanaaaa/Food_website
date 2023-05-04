547d63e07d32ab664f9463514ae8b4edc78b27dd
Boyana Dimitrova <boyanadimitrova11@gmail.com>
2023-05-04 20:03:43 +0300

    First Update

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import IoIosAddCircleOutline from 'react-icons/io';
+
+function Navbar() {
+  return (
+    <div>
+        <div>
+            <IoIosAddCircleOutline />
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
