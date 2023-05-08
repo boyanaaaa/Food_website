da9328cc824fbafe323c4961d88bda27cbaa130f
Boyana Dimitrova <boyanadimitrova11@gmail.com>
2023-05-08 10:38:03 +0300

    Cards section added.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar.js';
+import Hero from './components/Hero.js';
+import HeadLineCards from './components/HeadLineCards.js';
 
 function App() {
   return (
     <div className="App">
      <Navbar />
+     <Hero />
+     <HeadLineCards />
     </div>
   );
 }
